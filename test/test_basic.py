e2db5d6478bcce9c5771c61e31741a3db0b62a31
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2025-10-18 14:44:01 +0530

    Update tests to assert normal and raw URLs

--- test/test_basic.py
+++ test/test_basic.py
@@ -3,12 +3,14 @@ from yaso_paste import paste_to_yaso, YasoPasteError
 
 @pytest.mark.asyncio
 async def test_text_paste():
-    url = await paste_to_yaso("Hello World!", "txt")
-    assert url.startswith("https://yaso.su/raw/")
+    normal_url, raw_url = await paste_to_yaso("Hello World!", "txt")
+    assert normal_url.startswith("https://yaso.su/")
+    assert raw_url.startswith("https://yaso.su/raw/")
 
 @pytest.mark.asyncio
 async def test_file_paste(tmp_path):
     file = tmp_path / "example.txt"
     file.write_text("Hello File!")
-    url = await paste_to_yaso(file)
-    assert url.startswith("https://yaso.su/raw/")
+    normal_url, raw_url = await paste_to_yaso(file)
+    assert normal_url.startswith("https://yaso.su/")
+    assert raw_url.startswith("https://yaso.su/raw/")
